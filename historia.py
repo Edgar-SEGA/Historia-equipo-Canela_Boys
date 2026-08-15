ac380793fc9c0f09963b96ada3d37be9fd7bc023
Isaac <jimesp3@gmail.com>
2026-08-15 01:04:14 -0600

    Agrega partes de la Persona 2 y finaliza historia de Avengers

--- historia.py
+++ historia.py
@@ -1,12 +1,11 @@
 print("Thanos llegó a la Tierra exigiendo todas las gemas del infinito y un buen taco al pastor.")
-# 2 (Persona 2)
+print("Iron Man lo miró fijamente, bajó sus gafas y le dijo: 'Hoy no fiamos, mañana sí'.")
 print("De repente, Thor descendió del cielo con rayos, pero tropezó con su martillo y cayó sobre un auto.")
 
 print("El Capitán América suspiró, sacó su escudo y se puso a dirigir el tráfico para evitar más accidentes.")
-# 5 (Persona 2)
+print("Hulk, enojado porque no había más tacos, empezó a aplastar los puestos de comida cercanos.")
 print("Spider-Man intentó calmar a Hulk lanzándole telarañas, pero solo logró enredar a Ant-Man por error.")
 
 print("Thanos, confundido por el caos, decidió sentarse en la banqueta a esperar que los Avengers se organizaran.")
-# 8 (Persona 2)
-
+print("Doctor Strange abrió un portal mágico y trajo al verdadero héroe: un taquero con tres trompos de carne.")
 print("Al final, todos comieron juntos y Thanos olvidó su malvado plan de destruir la mitad del universo.")
