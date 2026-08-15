c613fd23418a3cc5fdd8279e5c58accf06a3d59d
Eduardo-Serv <2025310398@uteq.edu.mx>
2026-08-15 00:52:54 -0600

    Agrega lineas de Persona 3

--- historia.py
+++ historia.py
@@ -1,9 +1,12 @@
 print("Thanos llegó a la Tierra exigiendo todas las gemas del infinito y un buen taco al pastor.")
 # 2 (Persona 2)
-# 3 (Persona 3)
+print("De repente, Thor descendió del cielo con rayos, pero tropezó con su martillo y cayó sobre un auto.")
+
 print("El Capitán América suspiró, sacó su escudo y se puso a dirigir el tráfico para evitar más accidentes.")
 # 5 (Persona 2)
-# 6 (Persona 3)
+print("Spider-Man intentó calmar a Hulk lanzándole telarañas, pero solo logró enredar a Ant-Man por error.")
+
 print("Thanos, confundido por el caos, decidió sentarse en la banqueta a esperar que los Avengers se organizaran.")
 # 8 (Persona 2)
-# 9 (Persona 3)
+
+print("Al final, todos comieron juntos y Thanos olvidó su malvado plan de destruir la mitad del universo.")
