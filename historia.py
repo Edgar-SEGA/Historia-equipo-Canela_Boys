df133daacf52c80cee504646d21192b486a2f244
Eduardo-Serv <2025310398@uteq.edu.mx>
2026-08-15 00:33:14 -0600

    Agrega lineas de Persona 3

--- historia.py
+++ historia.py
@@ -1,1 +1,6 @@
  print("Hola soy pepe el grillo")
+print("3 (Persona 3): De repente, Thor descendió del cielo con rayos, pero tropezó con su martillo y cayó sobre un auto.")
+
+print("6 (Persona 3): Spider-Man intentó calmar a Hulk lanzándole telarañas, pero solo logró enredar a Ant-Man por error.")
+
+print("9 (Persona 3): Al final, todos comieron juntos y Thanos olvidó su malvado plan de destruir la mitad del universo.")
