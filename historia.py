8bb51907000ca61c5f1f7b3e829f18283a626905
Edgar <segundoedgar585@gmail.com>
2026-08-15 00:21:32 -0600

    Inicia historia de Avengers con líneas de Persona 1

--- historia.py
+++ historia.py
@@ -1,6 +1,9 @@
- print("Hola soy pepe el grillo")
-print("3 (Persona 3): De repente, Thor descendió del cielo con rayos, pero tropezó con su martillo y cayó sobre un auto.")
-
-print("6 (Persona 3): Spider-Man intentó calmar a Hulk lanzándole telarañas, pero solo logró enredar a Ant-Man por error.")
-
-print("9 (Persona 3): Al final, todos comieron juntos y Thanos olvidó su malvado plan de destruir la mitad del universo.")
+print("1 (Persona 1): Thanos llegó a la Tierra exigiendo todas las gemas del infinito y un buen taco al pastor.")
+# 2 (Persona 2)
+# 3 (Persona 3)
+print("4 (Persona 1): El Capitán América suspiró, sacó su escudo y se puso a dirigir el tráfico para evitar más accidentes.")
+# 5 (Persona 2)
+# 6 (Persona 3)
+print("7 (Persona 1): Thanos, confundido por el caos, decidió sentarse en la banqueta a esperar que los Avengers se organizaran.")
+# 8 (Persona 2)
+# 9 (Persona 3)
